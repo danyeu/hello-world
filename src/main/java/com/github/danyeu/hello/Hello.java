db3360f64148b3eb541ba145f519d5c53d1b0cf6
danyeu <io.daniel@outlook.com>
2024-06-04 11:23:37 +0100

    add comment

--- src/main/java/com/github/danyeu/hello/Hello.java
+++ src/main/java/com/github/danyeu/hello/Hello.java
@@ -4,5 +4,6 @@ package com.github.danyeu.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        // mvn clean install jib:dockerBuild
     }
 }
